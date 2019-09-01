4daccb8ae691490c9acf5a4d801e8aa6bb39066e
ppasham <prashanth@Prashanths-MacBook-Pro.local>
2019-08-31 23:53:44 -0400

    Initial checkin

---
+++ Learn/Angular/ngv8/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app.header.component';
+import { AppFooterComponent } from './app-footer/app.footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
